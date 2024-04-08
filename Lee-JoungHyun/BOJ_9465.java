6bd4be3f13678229006157be9e149c0a72140fe3
Lee-JoungHyun <doublehyun98@gmail.com>
2024-04-08 13:47:09 +0900

    스티커 / 20분

--- Lee-JoungHyun/BOJ_9465.java
+++ Lee-JoungHyun/BOJ_9465.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_9465 {
@@ -40,8 +39,8 @@ public class BOJ_9465 {
             dp[1][1] = arr[1][1] + arr[0][0];
 
             for (int i = 2; i < N; i++) {
-                dp[i][0] = arr[i][0] + Math.max(Math.max(dp[i - 2][0], dp[i - 2][1]), dp[i - 1][1]);
-                dp[i][1] = arr[i][1] + Math.max(Math.max(dp[i - 2][0], dp[i - 2][1]), dp[i - 1][0]);
+                dp[i][0] = arr[i][0] + Math.max(dp[i - 2][1], dp[i - 1][1]);
+                dp[i][1] = arr[i][1] + Math.max(dp[i - 2][0], dp[i - 1][0]);
             }
 
             sb.append(Math.max(dp[N - 1][0], dp[N - 1][1])).append("\n");
