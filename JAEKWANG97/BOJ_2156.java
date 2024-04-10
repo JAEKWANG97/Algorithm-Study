fa7705a29be1a0d53eb0c6cbd59c5b114e6b0491
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2024-04-10 20:32:59 +0900

    포도주 시식 / 60분

--- JAEKWANG97/BOJ_2156.java
+++ JAEKWANG97/BOJ_2156.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         init();
         dp();
-    }
+    } 
 
     private static void init() throws IOException {
         N = Integer.parseInt(br.readLine());
