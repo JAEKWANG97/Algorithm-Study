629a75937b73a3d312de57d9313519ed6606a57b
이학현 <gkrgus008@nate.com>
2024-05-04 01:09:48 +0900

    더 맵게 / 30분

---
+++ hyeonhakyi/PGS_42626.java
@@ -0,0 +1,29 @@
+package ex0504;
+
+import java.util.*;
+
+public class PGS_42626 {
+    class Solution {
+        public int solution(int[] scoville, int K) {
+            int answer = 0;
+            PriorityQueue<Integer> que = new PriorityQueue<>();
+
+            for(int i : scoville){
+                que.offer(i);
+            }
+
+            while(que.peek() < K){
+                if(que.size() == 1){
+                    return -1;
+                }
+
+                int fir = que.poll();
+                int sec = que.poll();
+                int sco = fir + (sec * 2);
+                que.offer(sco);
+                answer++;
+            }
+            return answer;
+        }
+    }
+}
