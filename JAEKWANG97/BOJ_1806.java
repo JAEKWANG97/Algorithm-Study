18b4c4ff14eb10b5a05a8920e42468b04300ff8c
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2024-05-09 10:28:19 +0900

    부분합 / 50분

---
+++ JAEKWANG97/BOJ_1806.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, S;
+    static long[] prefixSum;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        searchSize();
+    }
+
+    private static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        prefixSum = new long[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            long item = Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i - 1] + item;
+        }
+    }
+
+    private static void searchSize() {
+
+        int start = 0;
+        int end = 0;
+        long currentSum = 0;
+        int minLength = N + 1;
+        while (end <= N) {
+            currentSum = prefixSum[end] - prefixSum[start];
+            if (currentSum >= S) {
+                minLength = Math.min(minLength, end - start);
+                start++;
+            } else {
+                end++;
+            }
+        }
+
+        if (minLength == N + 1)
+            System.out.println(0);
+        else
+            System.out.println(minLength);
+    }
+}
